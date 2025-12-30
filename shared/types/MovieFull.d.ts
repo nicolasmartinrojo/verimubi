c6a0d53d000e2555b4e3078ce384ffad910b86f8
Nicolás Martín Rojo <nicolas.martin.rojo@gmail.com>
2025-12-30 07:19:40 -0300

    add moviefull type

---
+++ shared/types/MovieFull.d.ts
@@ -0,0 +1,25 @@
+type MovieFull = Movie & {
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Ratings: {
+    Source: string;
+    Value: string;
+  }[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
+};
