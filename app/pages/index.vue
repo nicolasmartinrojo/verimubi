93337c7d10903278993490fc4ffaaea13357c1af
Nicolás Martín Rojo <nicolas.martin.rojo@gmail.com>
2025-12-30 18:26:46 -0300

    move dashboard/ to home/

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,31 +1,79 @@
 <template>
   <div>
-    <TitlePage title="Home" />
-    <pre>
-    <!-- {{ batman }} -->
-    </pre>
-    <hr style="border: 1px solid black" />
-    <ul v-if="movies">
-      <li v-for="movie in movies.Search" :key="movie.imdbID">
-        <pre>
-            <NuxtLink :to="`/movie/${movie.imdbID}`">GOGOGO</NuxtLink>
-            {{ movie }}
-        </pre>
-      </li>
-    </ul>
-    <UiButton />
+    <UPageHero
+      title="Verimubi"
+      description="A necessary product for movie searching and move forward in the Verifarma selection process"
+      class="py-1"
+      :ui="{
+        container:
+          'flex flex-col lg:grid py-4 sm:py-32 lg:py-4 gap-16 sm:gap-y-24',
+      }"
+    >
+      <form @submit.prevent="onSubmit">
+        <UContainer class="flex flex-row">
+          <div class="basis-5/6">
+            <UInput
+              size="xl"
+              placeholder="Write some keywords to search for..."
+              class="w-full"
+              v-model="search"
+            />
+          </div>
+          <div class="basis-1/6 flex px-1">
+            <UButton
+              type="submit"
+              size="xl"
+              class="w-full flex fit-content flex justify-center"
+              @click="onSubmit"
+            >
+              Search
+            </UButton>
+          </div>
+        </UContainer>
+      </form>
+    </UPageHero>
+    <UContainer>
+      <UPageGrid>
+        <UBlogPost v-for="(card, index) in cards" v-bind="card" :key="index" />
+      </UPageGrid>
+      <UAlert
+        color="error"
+        variant="subtle"
+        title="Heads up!"
+        :description="errorMessage"
+        v-if="errorMessage"
+        icon="i-lucide-terminal"
+      />
+    </UContainer>
   </div>
 </template>
-
 <script setup lang="ts">
-// const { data: batman } = await useFetch(
-//   "https://www.omdbapi.com/?i=tt4116284&apikey=c6bfa3ef"
-// );
+import type { BlogPostProps } from "@nuxt/ui";
 
 interface MoviesRes {
   Search: Movie[];
 }
-const { data: movies } = await useFetch<MoviesRes>("/api/movies");
-</script>
+const cards = ref<BlogPostProps[]>([]);
 
-<style lang="scss" scoped></style>
+const search = ref("");
+const errorMessage = ref("");
+const onSubmit = async () => {
+  try {
+    const { data: resMovies } = await useFetch<MoviesRes>("/api/movies", {
+      query: { search: search.value },
+    });
+    if (!resMovies.value?.Search) return;
+
+    cards.value = resMovies.value?.Search.map((resMovie) => ({
+      title: resMovie.Title,
+      date: resMovie.Year,
+      image: resMovie.Poster,
+      to: `/movie/${resMovie.imdbID}`,
+    }));
+  } catch (error) {
+    debugger;
+    const err = error as { Response: string; Error: string };
+    errorMessage.value = err.Error as string;
+  }
+};
+</script>
