ad099fa9efa91dbe94b07df4997bf46a10cc0a4b
Nicolás Martín Rojo <nicolas.martin.rojo@gmail.com>
2025-12-30 06:27:36 -0300

    modeify template title

--- app/app.vue
+++ app/app.vue
@@ -59,17 +59,7 @@ useHead({
   },
 });
 
-const title = "Nuxt Starter Template";
-const description =
-  "A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.";
-
 useSeoMeta({
-  title,
-  description,
-  ogTitle: title,
-  ogDescription: description,
-  ogImage: "https://ui.nuxt.com/assets/templates/nuxt/starter-light.png",
-  twitterImage: "https://ui.nuxt.com/assets/templates/nuxt/starter-light.png",
-  twitterCard: "summary_large_image",
+  title: "Verimubi - verifarma",
 });
 </script>
