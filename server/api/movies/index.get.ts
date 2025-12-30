ebd5f206a4d5a6942de2e61235637850652cff50
Nicolás Martín Rojo <nicolas.martin.rojo@gmail.com>
2025-12-30 06:27:29 -0300

    make api search on server

--- server/api/movies/index.get.ts
+++ server/api/movies/index.get.ts
@@ -2,9 +2,10 @@ interface MoviesRes {
   Search: Movie[];
 }
 
-export default eventHandler(async () => {
+export default eventHandler(async (event) => {
+  const { search } = getQuery(event);
   const data = await $fetch<MoviesRes>(
-    "https://www.omdbapi.com/?s=batman&apikey=c6bfa3ef"
+    `https://www.omdbapi.com/?s=${search}&apikey=c6bfa3ef`
   );
   return data;
 });
